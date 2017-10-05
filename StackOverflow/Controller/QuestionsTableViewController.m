0a16fdb104e9c710ddb30d6e59260edacbf40ebc
Martha <malea.kotelo@gmail.com>
2017-10-05 10:41:06 +0200

    Initial commit

---
+++ StackOverflow/Controller/QuestionsTableViewController.m
@@ -0,0 +1,88 @@
+//
+//  QuestionsTableViewController.m
+//  StackOverflow
+//
+//  Created by Mali on 2017/10/04.
+//  Copyright © 2017 DVT. All rights reserved.
+//
+
+#import "QuestionsTableViewController.h"
+#import "QuestionsCell.h"
+#import "DataFetchDelegate.h"
+#import "QuestionsDataObject.h"
+#import "QuestionsData.h"
+
+
+@interface QuestionsTableViewController () <DataFetchDelegate> {
+    NSArray *dataResult;
+    QuestionsDataObject *dataObject;
+    UIActivityIndicatorView *_activityView;
+}
+
+@end
+
+@implementation QuestionsTableViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    dataObject = [[QuestionsDataObject alloc] init];
+    dataObject.questionRequest = [[QuestionRequest alloc] init];
+    dataObject.questionRequest.delegate = dataObject;
+    dataObject.delegate = self;
+    
+    [dataObject getData];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return dataResult.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    QuestionsCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+
+    QuestionsData *questionData = dataResult[indexPath.row];
+    
+    //Select image based on whether the answer is accepted or not
+    if (questionData.accepted_answer_id) {
+        cell.acceptedAnswerImage.image =  [UIImage imageNamed: @"greencircle.png"];
+    } else {
+        cell.acceptedAnswerImage.image =  [UIImage imageNamed: @"circle"];
+    }
+    
+    cell.questionTitle.text = questionData.title;
+    [cell.questionTitle sizeToFit];
+    cell.answerCount.text = [NSString stringWithFormat:@"%@", questionData.answer_count];
+    cell.tags.text =  [questionData.tags componentsJoinedByString:@" "];
+   
+    return cell;
+}
+
+- (void)didReceiveData:(NSArray *)data {
+    dataResult = data;
+   
+    dispatch_async(dispatch_get_main_queue(), ^{
+         [self.tableView reloadData];
+    });
+    [_activityView stopAnimating];
+}
+
+- (void)fetchingDataFailedWithError:(NSError *)error {
+    NSLog(@"Error %@; %@", error, [error localizedDescription]);
+}
+
+@end
